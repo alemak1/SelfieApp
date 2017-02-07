aca21d84e70f79c88ea687618314e4b17435d70f
yongrenwang <czie.alexmak@gmail.com>
2017-02-07 20:48:17 +0800

    Deleted main storyboard file, and programmatically created a UIWindow whose frame is initialized with the bounds of the main screen (obtained from UIScreen.main); subclassed a UIViewController to create a custom root view controller (i.e. photolist controller) to be set as the root view controller of a navigation controller, which in turn is assigned to the root view controller property of the window

--- SelfieApp/AppDelegate.swift
+++ SelfieApp/AppDelegate.swift
@@ -17,6 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        guard let window = window else { return false}
+        
+        window.backgroundColor = UIColor.white
+        
+        let photoListController = PhotoListController()
+        let navigationController = UINavigationController(rootViewController: photoListController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
         return true
     }
 
