cd0dbca074c8a87d9093230e5668362db399fd99
yongrenwang <czie.alexmak@gmail.com>
2017-02-07 20:57:46 +0800

    Declared and defined a button as lazy stored property on the PhotoListController class; the button is instantiated and customized with a closure; also, added an event handler to the button using addTarget, which calls presentImagePickerController on the photo list controller

--- SelfieApp/PhotoListController.swift
+++ SelfieApp/PhotoListController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class PhotoListController: UIViewController {
 
+    lazy var cameraButton: UIButton = {
+        let button = UIButton(type: UIButtonType.system)
+        
+        button.setTitle("Camera", for: .normal)
+        button.tintColor = UIColor.white
+        button.backgroundColor = UIColor(red: 254/255.0, green: 123/255.0, blue: 135/255.0, alpha: 1.0)
+        
+        button.addTarget(self, action: #selector(PhotoListController.presentImagePickerController), for: .touchUpInside)
+        
+        return button
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,8 @@ class PhotoListController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc private func presentImagePickerController() {
+        
+    }
 }
 
