1aa522cb30f3190b4ac103998e3dec1a67d31f04
yongrenwang <czie.alexmak@gmail.com>
2017-02-08 20:42:20 +0800

    Finished implementing FilteredImageBuilder class

--- SelfieApp/FilteredImageBuilder.swift
+++ SelfieApp/FilteredImageBuilder.swift
@@ -8,36 +8,67 @@
 
 import Foundation
 import CoreImage
+import UIKit
 
-struct PhotoFilter{
+
+
+final class FilteredImageBuilder{
+    private let image: UIImage
+
     
-    static let ColorClamp = "CIColorClamp"
-    static let ColorControls = "CIColorControls"
-    static let PhotoEffectInstant = "CIPhotoEffectInstant"
-    static let PhotoEffectProcess = "CIPhotoEffectProcess"
-    static let PhotoEffectNoir = "CIPhotoEffectNoir"
-    static let Sepia = "CISepiaTone"
+    private struct PhotoFilter{
+        
+        static let ColorClamp = "CIColorClamp"
+        static let ColorControls = "CIColorControls"
+        static let PhotoEffectInstant = "CIPhotoEffectInstant"
+        static let PhotoEffectProcess = "CIPhotoEffectProcess"
+        static let PhotoEffectNoir = "CIPhotoEffectNoir"
+        static let Sepia = "CISepiaTone"
+        
+        static func defaultFilters() -> [CIFilter]{
+            
+            //Color Clamp
+            let colorClamp = CIFilter(name: PhotoFilter.ColorClamp)!
+            colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
+            colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
+            
+            //Color Controls
+            let colorControls = CIFilter(name: PhotoFilter.ColorControls)!
+            colorControls.setValue(0.1, forKey: kCIInputSaturationKey)
+            
+            //Photo Effects
+            let photoEffectInstant = CIFilter(name: PhotoFilter.PhotoEffectInstant)!
+            let photoEffectProcess = CIFilter(name: PhotoFilter.PhotoEffectProcess)!
+            let photoEffectNoir = CIFilter(name: PhotoFilter.PhotoEffectNoir)!
+            
+            //Sepia Tone
+            let sepia = CIFilter(name: PhotoFilter.Sepia)!
+            sepia.setValue(0.7, forKey: kCIInputIntensityKey)
+            
+            return [colorClamp, colorControls, photoEffectInstant, photoEffectProcess, photoEffectNoir,sepia]
+        }
+        
+        
+    }
     
-    static func defaultFilter() -> [CIFilter]{
+    init(image: UIImage){
+        self.image = image
+    }
+    
+    func imageWithDefaultFilters() -> [UIImage]{
+        return image(image(withFilters: PhotoFilter.defaultFilters()))
+    }
+    
+    func image(withFilters filter: [CIFilter]) -> [UIImage]{
+        return filters.map { image(self.image, withFilter: $0) }
+    }
+    
+    func image(image: UIImage, withFilter filter: CIFilter) -> UIImage{
+        let inputImage = image.ciImage ?? CIImage(image: image)!
+        filter.setValue(inputImage, forKey: kCIInputImageKey)
         
-        //Color Clamp
-        let colorClamp = CIFilter(name: PhotoFilter.ColorClamp)!
-        colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
-        colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
+        let outputImage = filter.outputImage!
         
-        //Color Controls
-        let colorControls = CIFilter(name: PhotoFilter.ColorControls)!
-        colorControls.setValue(0.1, forKey: kCIInputSaturationKey)
-        
-        //Photo Effects
-        let photoEffectInstant = CIFilter(name: PhotoFilter.PhotoEffectInstant)!
-        let photoEffectProcess = CIFilter(name: PhotoFilter.PhotoEffectProcess)!
-        let photoEffectNoir = CIFilter(name: PhotoFilter.PhotoEffectNoir)!
-        
-        //Sepia Tone
-        let sepia = CIFilter(name: PhotoFilter.Sepia)!
-        sepia.setValue(0.7, forKey: kCIInputIntensityKey)
-        
-        return [colorClamp, colorControls, photoEffectInstant, photoEffectProcess, photoEffectNoir,sepia]
+        return UIImage(ciImage: outputImage)
     }
 }
