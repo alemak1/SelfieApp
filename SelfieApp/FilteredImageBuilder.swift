115e34ddc76b08f8a5aba75b8f078c957246a0aa
yongrenwang <czie.alexmak@gmail.com>
2017-02-08 20:27:08 +0800

    Finished adding all filters to the wrapper struct PhotoFilter whose class method defaultFilter() returns an array of filters representing a restricted range of filter from all filters available

--- SelfieApp/FilteredImageBuilder.swift
+++ SelfieApp/FilteredImageBuilder.swift
@@ -12,11 +12,32 @@ import CoreImage
 struct PhotoFilter{
     
     static let ColorClamp = "CIColorClamp"
+    static let ColorControls = "CIColorControls"
+    static let PhotoEffectInstant = "CIPhotoEffectInstant"
+    static let PhotoEffectProcess = "CIPhotoEffectProcess"
+    static let PhotoEffectNoir = "CIPhotoEffectNoir"
+    static let Sepia = "CISepiaTone"
     
     static func defaultFilter() -> [CIFilter]{
+        
+        //Color Clamp
         let colorClamp = CIFilter(name: PhotoFilter.ColorClamp)!
         colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
         colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
-        return [colorClamp]
+        
+        //Color Controls
+        let colorControls = CIFilter(name: PhotoFilter.ColorControls)!
+        colorControls.setValue(0.1, forKey: kCIInputSaturationKey)
+        
+        //Photo Effects
+        let photoEffectInstant = CIFilter(name: PhotoFilter.PhotoEffectInstant)!
+        let photoEffectProcess = CIFilter(name: PhotoFilter.PhotoEffectProcess)!
+        let photoEffectNoir = CIFilter(name: PhotoFilter.PhotoEffectNoir)!
+        
+        //Sepia Tone
+        let sepia = CIFilter(name: PhotoFilter.Sepia)!
+        sepia.setValue(0.7, forKey: kCIInputIntensityKey)
+        
+        return [colorClamp, colorControls, photoEffectInstant, photoEffectProcess, photoEffectNoir,sepia]
     }
 }
