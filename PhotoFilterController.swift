64e2d50bcf18328f4d21d6f3493feb2dcecf394c
yongrenwang <czie.alexmak@gmail.com>
2017-02-08 19:20:40 +0800

    Created new groups for Model, View, Controller, and Helpers, and organized files into appropriate directories

---
+++ PhotoFilterController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoFilterController.swift
+//  SelfieApp
+//
+//  Created by Aleksander Makedonski on 2/8/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+
+
+class PhotoFilterController: UIViewController{
+    
+}
